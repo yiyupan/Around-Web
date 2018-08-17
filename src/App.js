53a70f20fbfdae78b21e39fa83709ae6ddad1ff2
yiyupan <yypan14@gmail.com>
2018-08-16 18:40:24 -0700

    install antd and jquery, add Main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Header} from './Header';
+import { Main} from './Main'
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
-
+        <Main/>
       </div>
     );
   }
