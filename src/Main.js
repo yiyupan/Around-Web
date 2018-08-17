f5028b8b75f6035599bad8bfd2b06f906d2568e5
yiyupan <yypan14@gmail.com>
2018-08-16 19:06:05 -0700

    add register page

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { Register} from './Register'
 export class Main extends React.Component {
     render () {
         return (
             <div>
-                this is main
+                <Register/>
             </div>
         );
     }
