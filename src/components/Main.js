83aa6f30e52e5f2d1a6dd91db8f1618ebf4f6039
yiyupan <yypan14@gmail.com>
2018-08-19 17:53:55 -0700

    redirect to home, root

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,12 +9,19 @@ export class Main extends React.Component {
         return this.props.isLoggedIn ? <Redirect to="/home"/> : <Login handleLogin={this.props.handleLogin}/>;
     }
 
+    getHome = () => {
+        return this.props.isLoggedIn ? <Home/> : <Redirect to="/login"/>;
+    }
+
+    getRoot = () => {
+        return <Redirect to="/login"/>;
+    }
     render () {
         return (
             <div className="main">
                 <Switch>
-                    <Route exact path="/" render={this.getLogin}/>
-                    <Route path="/home" component={Home}/>
+                    <Route exact path="/" render={this.getRoot}/>
+                    <Route path="/home" render={this.getHome}/>
                     <Route path="/login" render={this.getLogin}/>
                     <Route path="/register" component={Register}/>
                     <Route render={this.getLogin}/>
